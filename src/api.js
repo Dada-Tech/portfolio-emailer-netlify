926b299545efc8c72a9cb74e321c1c26e893608e
dada-tech <daviddadaa@hotmail.com>
2019-08-20 00:22:21 -0600

    code cleanup

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ const serverless = require("serverless-http");
 const Joi = require('@hapi/joi'); // class returned
 require('dotenv').config();
 
-
 const app = express();
 const router = express.Router();
 
@@ -16,8 +15,7 @@ app.use(express.json());
 
 
 router.get("/", (req, res) => {
-  res.send(process.env.SENDGRID_API_KEY);
-  // res.send("ok for now");
+  res.send("Hello World");
 });
 
 router.post("/mail", (req, res) => {
@@ -33,8 +31,8 @@ router.post("/mail", (req, res) => {
     return  res.status(400).send(result.error.details[0].message);
 
   const msg = {
-    to: 'daviddadaa@hotmail.com',
-    from: 'david-dada-portfolio@dadadavid.com',
+    to: 'example@example.com',
+    from: 'SOME-EMAIL@mail.com',
     subject: req.body.subject.toString(),
     text: req.body.text,
     html: '<strong>sent from Netlify Server</strong>',
@@ -59,38 +57,6 @@ router.post("/mail", (req, res) => {
 
   res.send("email SENT");
 });
-
-
-
-router.get("/mail", (req, res) => {
-  const msg = {
-    to: 'daviddadaa@hotmail.com',
-    from: 'david-dada-portfolio@dadadavid.com',
-    subject: "testing netlify CLOUD",
-    text: "CLOUD",
-    html: '<strong>sent from Netlify ONLINE Server</strong>',
-  };
-
-  sgMail
-      .send(msg)
-      .then(() => {
-        //Celebrate
-        // console.log('Email Sent!');
-      })
-      .catch(error => {
-        //Log friendly error
-        console.error(error.toString());
-
-        //Extract error msg
-        const {message, code, response} = error;
-
-        //Extract response msg
-        const {headers, body} = response;
-      });
-
-  res.send("email SENTT");
-});
-
 
 app.use(`/.netlify/functions/api`, router);
 
